98f3fdc47a45bccb41c29ff697b8b3d75d8808a4
zekiblue <hi.zekiblue@gmail.com>
2024-04-17 18:56:41 +0200

    :bug: Fix deployment

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,5 @@ export * from "./AddressInput";
 export * from "./DarkModeButton";
 export * from "./Footer";
 export * from "./Header";
-export * from "./MainPane";
 export * from "./InfoText";
-export * from "./DepositPane";
 export * from "./PositionPane";
-export * from "./DemoPane";
